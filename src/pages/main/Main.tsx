6e641866000215bc38b75b81d75e7b039c3fdd14
Uyama <axelezgames@gmail.com>
2025-01-16 00:56:36 +0500

    chore(cleanup): remove console log and fix base url

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,8 +1,6 @@
 import { CatsList } from '@/components/catsList/CatsList';
 
 export const Main: React.FC = () => {
-  console.log('hello');
-
   return (
     <>
       <CatsList />
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  base: "/vk-frontend-challenge"
+  base: '/vk-frontend-challenge/',
 });
